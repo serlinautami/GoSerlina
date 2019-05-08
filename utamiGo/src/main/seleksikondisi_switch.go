b1ddb18745ddc2ad531739f9ce2973bf1a3cf58c
serlina utami <serlinautami93@gmail.com>
2019-05-08 12:15:12 +0700

    dasar_pemerograman_golang

---
+++ utamiGo/src/main/seleksikondisi_switch.go
@@ -0,0 +1,20 @@
+package main
+
+import 
+	"fmt"
+
+func main() {
+	var eko = 1
+
+	switch eko {
+		case 2:
+    		fmt.Println("eko naik sepeda")
+		case 3:
+    		fmt.Println("eko tidak naik sepeda")
+		default:
+    		fmt.Println("emang iya???")
+    }
+}
+
+	
+
